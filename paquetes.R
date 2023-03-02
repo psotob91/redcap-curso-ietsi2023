fa6e1727eea6ae65cb030c08d3377d0d5b49e32a
psotob <percys1991@gmail.com>
2023-03-01 22:07:31 -0500

    Update

---
+++ paquetes.R
@@ -0,0 +1,27 @@
+# if (!require(pacman)) {
+#   install.packages("pacman")
+# }
+# 
+# pacman::p_load(here, 
+#                DescTools,
+#                ggpubr,
+#                ggsurvfit,
+#                naniar,
+#                plotly,
+#                psych,
+#                summarytools,
+#                survminer,
+#                treemap,
+#                VIM,
+#                visdat,
+#                webshot2, 
+#                htmltools, 
+#                knitr)
+# 
+# pacman::p_load(gtsummary, flextable, huxtable, survival, survminer, ggsurvfit)
+# 
+# remotes::install_github("matt-dray/quartostamp")
+# 
+# install.packages(
+#   "datapasta",
+#   repos = c(mm = "https://milesmcbain.r-universe.dev", getOption("repos")))
